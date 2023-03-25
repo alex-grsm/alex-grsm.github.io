17cb666186a0cd5aa033381f352a6fbcd6f04bfc
Alex Grsm <alexey.grsm@gmail.com>
2023-03-25 15:32:44 +0200

    optimize

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('install', function(event) {
           '/img/*',
           '/fonts/*'
         ]).catch(function(error) {
-            console.error('Error adding resources to cache:', error);
+          console.error('Error adding resources to cache:', error);
         });
       })
     );
@@ -18,8 +18,36 @@ self.addEventListener('install', function(event) {
   // Fetch resources from cache first, and fallback to network
   self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+      caches.open('my-cache').then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          // Check if the response exists in the cache and return it
+          if (response) {
+            return response;
+          }
+  
+          // Fetch the resource from the network and cache it for 30 days
+          return fetch(event.request).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        });
+      })
+    );
+  });
+  
+  // Set the cache expiration time to 30 days
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            return caches.open(cacheName).then(function(cache) {
+              cache.delete(event.request, {
+                ignoreSearch: true
+              });
+            });
+          })
+        );
       })
     );
   });
